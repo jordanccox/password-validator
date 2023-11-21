f02b1fbda31e6c8cd63a3d7dfe696067a9ac11a3
Jordan Cox <jordancox747@outlook.com>
2023-11-21 05:06:19 -0700

    toggle password show working

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [password, setPassword] = useState("");
+  const [checked, setChecked] = useState(false);
+  const [showPassword, setShowPassword] = useState("password");
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const toggleShowPassword = (event) => {
+    
+    if (event.target.value == "false") {
+      setShowPassword("text");
+      setChecked(true);
+    } else {
+      setShowPassword("password");
+      setChecked(false);
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container className="text-light">
+        <Row>
+          <Col>
+            <h1 className="text-center mt-3">Validate a Password</h1>
+          </Col>
+        </Row>
+        <hr />
+        <Row>
+          <Form>
+            <Form.Group className="mb-3" controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type={showPassword}
+                placeholder="Enter a password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+              <Form.Check type="checkbox">
+                <Form.Check.Input
+                  type="checkbox"
+                  id="showPassword"
+                  label="Show password"
+                  onClick={toggleShowPassword}
+                  value={checked}
+                />
+                <Form.Check.Label>Show password</Form.Check.Label>
+              </Form.Check>
+              <Form.Text className="danger">Error message goes here</Form.Text>
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Try Password
+            </Button>
+          </Form>
+        </Row>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
