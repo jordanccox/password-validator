81f5ac4a8391e13058391eb9b275363157db1196
Jordan Cox <jordancox747@outlook.com>
2023-11-21 06:22:08 -0700

    progress on app

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,111 @@ import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
+// Helper functions
+
+const validatePasswordLength = (password) => {
+  if (password.length < 8 || password.length > 16) {
+    return false;
+  }
+
+  return true;
+};
+
+const validatePasswordCharacters = (password) => {
+  const allowedCharacters = /^[A-Za-z0-9]*$/;
+
+  const match = password.match(allowedCharacters);
+
+  if (!match) {
+    return false;
+  }
+
+  return true;
+};
+
+const validateAtLeastOneDigit = (password) => {
+  const digitRegex = /[0-9]+/g;
+
+  const foundDigit = password.match(digitRegex);
+
+  if (!foundDigit) {
+    return false;
+  }
+
+  return true;
+};
+
 function App() {
   const [password, setPassword] = useState("");
   const [checked, setChecked] = useState(false);
   const [showPassword, setShowPassword] = useState("password");
+  const [validPassword, setValidPassword] = useState(null);
+  const [errors, setErrors] = useState({});
 
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
   };
 
   const toggleShowPassword = (event) => {
-    
     if (event.target.value == "false") {
       setShowPassword("text");
       setChecked(true);
     } else {
       setShowPassword("password");
       setChecked(false);
+    }
+  };
+
+  const handleTryPassword = () => { 
+    event.preventDefault();
+    setErrors({});
+
+    if (validatePasswordLength(password) === false) {
+      setErrors({
+        message: "Password must be between 8 and 16 characters"
+      });
+      return;
+    }
+
+    if (validatePasswordCharacters(password) === false) {
+      setErrors({
+        message: "Password must only contain alphanumeric characters"
+      });
+      return;
+    }
+
+    if (validateAtLeastOneDigit(password) === false) {
+      setErrors({
+        message: "Password must contain at least one digit"
+      });
+      return;
+    }
+  };
+
+  const searchDictionary = async () => {
+    event.preventDefault();
+    setErrors({});
+
+    const searchTerm = password;
+    console.log(searchTerm);
+
+    try {
+      const response = await fetch(
+        `https://api.dictionaryapi.dev/api/v2/entries/en/${searchTerm}`
+      );
+
+      if (response.status === 200) {
+        setErrors({
+          message: "Password cannot be a word found in the English dictionary",
+        });
+      }
+
+      if (response.status === 404) {
+        setValidPassword(searchTerm);
+        console.log(validPassword);
+      }
+    } catch (err) {
+      console.error(err);
     }
   };
 
@@ -43,8 +131,16 @@ function App() {
                 placeholder="Enter a password"
                 value={password}
                 onChange={handlePasswordChange}
+                className="mb-2"
               />
-              <Form.Check type="checkbox">
+              {errors.message ? (
+                <Form.Text className="bg-danger text-light ps-2 pe-2">
+                  {errors.message}
+                </Form.Text>
+              ) : (
+                ""
+              )}
+              <Form.Check type="checkbox" className="mt-2">
                 <Form.Check.Input
                   type="checkbox"
                   id="showPassword"
@@ -54,12 +150,15 @@ function App() {
                 />
                 <Form.Check.Label>Show password</Form.Check.Label>
               </Form.Check>
-              <Form.Text className="danger">Error message goes here</Form.Text>
+              <Button
+                variant="primary"
+                type="submit"
+                className="mt-4"
+                onClick={handleTryPassword}
+              >
+                Try Password
+              </Button>
             </Form.Group>
-
-            <Button variant="primary" type="submit">
-              Try Password
-            </Button>
           </Form>
         </Row>
       </Container>
